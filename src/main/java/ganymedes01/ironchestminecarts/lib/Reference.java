1825fe1629fcc5347ffbcc77af261e5206debb41
Maya <10861407+serenibyss@users.noreply.github.com>
2025-04-19 11:03:33 -0500

    update build

--- src/main/java/ganymedes01/ironchestminecarts/lib/Reference.java
+++ src/main/java/ganymedes01/ironchestminecarts/lib/Reference.java
@@ -1,9 +1,11 @@
 package ganymedes01.ironchestminecarts.lib;
 
+import ganymedes01.ironchestminecarts.ICMCTags;
+
 public class Reference {
 
     public static final String MOD_ID = "ironchestminecarts";
     public static final String MOD_NAME = "Iron Chest Minecarts";
     public static final String DEPENDENCIES = "required-after:Forge@[10.10,);required-after:IronChest";
-    public static final String VERSION_NUMBER = "GRADLETOKEN_VERSION";
+    public static final String VERSION_NUMBER = ICMCTags.VERSION;
 }
